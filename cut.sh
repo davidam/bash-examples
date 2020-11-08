a2caff6f345a045176ecffbcef6a63327844759c
David Arroyo Menéndez <davidam@gmail.com>
2020-11-08 05:47:58 +0100

    cut.sh: generate echos explaining each cut

--- cut.sh
+++ cut.sh
@@ -16,35 +16,51 @@
 echo "cat state.txt"
 cat state.txt
 
+echo "Dado un fichero imprime los 3 primeros caracteres de cada línea"
 echo "cut -b 1,2,3 state.txt"
 cut -b 1,2,3 state.txt
 
+echo "Dado un fichero imprime los caracteres del 1 al 3 y del 5 al 7 de cada línea"
 echo "cut -b 1-3,5-7 state.txt"
 cut -b 1-3,5-7 state.txt
 
-echo "cut -b 1- state.txt"
-cut -b 1- state.txt
-
+echo "Dado un fichero imprime los 3 primeros caracteres de cada línea"
 echo "cut -b -3 state.txt"
 cut -b -3 state.txt
 
+echo "Dado un fichero imprime cada línea hasta el primer espacio en blanco"
+echo 'cut -d" " -f1 state.txt'
+cut -d" " -f1 state.txt
+
+echo "Dado un fichero imprime cada línea hasta la primera coma ignorando las líneas sin coma"
+echo 'cut -d"," -s -f1 state.txt'
+cut -d"," -f1 state.txt
+
+echo "Dado un fichero imprime cada línea desde la primera coma hasta la segunda coma ignorando las líneas sin coma"
+echo 'cut -d"," -s -f2 state.txt'
+cut -d"," -s -f2 state.txt
+
+echo "Dado un fichero imprime cada línea desde la primera coma hasta el fin de línea ignorando las líneas sin coma"
+echo 'cut -d"," -s -f 2- state.txt'
+cut -d"," -s -f 2- state.txt
+
+echo "Dado un fichero imprime los caracteres 2, 5 y 7 de cada línea"
 echo "cut -c 2,5,7 state.txt"
 cut -c 2,5,7 state.txt
 
+echo "Dado un fichero imprime los 7 primeros caracteres de cada línea"
 echo "cut -c 1-7 state.txt"
 cut -c 1-7 state.txt
 
-echo "cut --complement -d " " -f 1 state.txt"
-cut --complement -d " " -f 1 state.txt
+echo "Dado un fichero imprime los caracteres del 30 al 60 de cada línea"
+echo "cut -c 30-60 state.txt"
+cut -c 30-60 state.txt
 
+echo "Dado un fichero imprime todas las líneas y caracteres menos el 5"
 echo "cut --complement -c 5 state.txt"
 cut --complement -c 5 state.txt
 
-echo "cut -d " " -f 1,2 state.txt --output-delimiter='%'"
-cut -d " " -f 1,2 state.txt --output-delimiter='%'
+echo "Dado un fichero imprime cada línea excepto desde el inicio hasta el primer espacio en blanco"
+echo "cut --complement -d " " -f 1 state.txt"
+cut --complement -d " " -f 1 state.txt
 
-echo "cat state.txt | cut -d ' ' -f 1 | sort -r"
-cat state.txt | cut -d ' ' -f 1 | sort -r
-
-echo "cat state.txt | head -n 3 | cut -d ' ' -f 1 > list.txt"
-cat state.txt | head -n 3 | cut -d ' ' -f 1 > list.txt 
